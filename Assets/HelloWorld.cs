04ceea3baec24a29458f5ac60807c74e5f1ffbab
Victor Redondo Mula <victor.redondo@opendeusto.es>
2024-02-23 08:50:22 +0100

    namespace tryout

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using clasePlayer;
 
-public class HelloWorld : MonoBehaviour
-{
-    enemy enemy;
-    player player;
-    // Start is called before the first frame update
-    void Start()
+
+    public class HelloWorld : MonoBehaviour
     {
-        Debug.Log("Hello world");
+        enemy enemy;
+        player player;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Hello world");
 
-        player = new player("MainCharacter");
-        enemy = new enemy("Enemy");
+            player = new player("MainCharacter");
+            enemy = new enemy("Enemy");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log("La salud del jugador es de: " + player);
+            Debug.Log("El jugador ha eliminado: " + player.getKills());
+
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("La salud del jugador es de: " + player);
-        Debug.Log("El jugador ha eliminado: " + player.getKills());
-
-    }
-}
